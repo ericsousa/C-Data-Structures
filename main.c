96660553e7a087b4392221767f7eed686d2f6660
Eric Sousa <ericsousa@gmail.com>
2025-09-28 19:21:48 -0300

    feat:add Student struct definition

--- main.c
+++ main.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+typedef struct {
+    int id;
+    char name[50];
+    char course[50];
+    float grades[4];
+    int enrollmentYear;
+    int age;
+} Student;
+
 int main() {
     printf("C-Data-Structures");
     return 0;
